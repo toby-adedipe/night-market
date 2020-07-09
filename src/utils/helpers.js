2edc69b2c8d0251809cda1d93cc5daba3d7383d5
Adedipe Toby <toby_adedipe@yahoo.com>
2020-07-09 20:39:00 +0100

    quick link fix

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,5 @@
 //http://35.194.13.170
 export default function getTweets({ value, location }){
-    return fetch(`https://35.194.13.170/search?value=${value}&location=${location}`, {mode: 'cors'})
+    return fetch(`https://onestopshop-281920.uc.r.appspot.com/search?value=${value}&location=${location}`, {mode: 'cors'})
     .then(res=>res.json())
 }
